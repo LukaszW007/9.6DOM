f030858efc6d39a1f31ab5d7fa12f81587dea312
LukaszWiszniewski <wisznu07@gmail.com>
2017-12-06 14:25:55 +0100

    Fixed click listener

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 var addButton = document.getElementById('addElem'),
     list = document.getElementById('list');
 
-
 addButton.addEventListener('click', function () {
     var item = document.createElement('li'),
         count = document.getElementsByTagName('li');
-    item.innerHTML = 'item '+(count.length+1);
+    console.log(count.length);
+    item.innerHTML = 'item '+(count.length);
     list.appendChild(item);
 });
 
